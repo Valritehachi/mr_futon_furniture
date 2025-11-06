a0bc14f6cb79705412fca361f407ae1ed90f70b9
Valritehachi <valritehachi@gmail.com>
2025-11-06 15:02:59 -0500

    more changes

--- app/components/CommentForm.tsx
+++ app/components/CommentForm.tsx
@@ -73,7 +73,7 @@ const CommentForm: React.FC<CommentFormProps> = ({ articleId }) => {
 
 
     try {
-      const res = await fetch("/api/submit-comment", {
+      const res = await fetch("/api/submit_comment", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ articleId, name, comment, token }),
